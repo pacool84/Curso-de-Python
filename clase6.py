917c9489a4815597062407415fcc9b5a5c672c2f
Pacool84 <flopez_1384@hotmail.com>
2024-07-30 21:12:55 -0600

    [ADD] Clase de operaciones matematicas

---
+++ clase6.py
@@ -0,0 +1,43 @@
+#Operadores matematicos 
+
+a = 10
+b = 3
+
+print('Suma:', a+b)
+print('Resta:', a-b)
+print('Multiplicacion:', a*b)
+print('Division:', a/b)
+
+print('Modulo:', a%b)
+print('Parte entera de la division:', a//b)
+print('Potenciacion:', a**b)
+
+#Atajos o shortcuts
+a+=2
+print(a)
+
+b-=2
+print(b)
+
+# a/=2
+# a*=2
+
+#Consolidar el concepto PEMDAS
+
+#P -> Paréntesis
+
+#E -> Exponenciación
+
+#M -> Multiplicación
+
+#D -> División
+
+#A -> Adición
+
+#S -> Sustracción
+
+#Ejemplo
+
+operation = (2+3) * (4**2) / 8 - 1
+print(operation)
+
---
+++ clase6_2.py
@@ -0,0 +1,19 @@
+#Operaciones con comparaciones booleanas
+
+a = 10
+b = 4
+
+print(a>b)
+print(a<b)
+print(a >= b)
+print(a <= b)
+
+c = 8
+d = 8.0
+print(c>=d)
+print(c<=d) #Python toma como una igualdad aunque los tipos de datos sean diferente, int >= float
+
+e = 10
+f = 3
+print(e == f)
+print(e != f)
